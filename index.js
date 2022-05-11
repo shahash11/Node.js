9251c9e15581eea12a2c6605d285f3050bab7a11
shahash11 <71052668+shahash11@users.noreply.github.com>
2022-05-11 15:20:11 -0500

    Basics of Node.js and global identifiers

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log("shahash")
+
+// global identifiers
+
+// 1). global----it is used for setting global values in node.js
+
+// console.log(global)
+
+// gloabl.place === place
+// place = "Nepal"  
+// city = "Kathmandu"
+
+// function print(){
+//     console.log(place)
+//     console.log(city)
+// }
+
+// print()
+
+// 2). process ---------- provides interaction with current Node.js proccess
+
+// console.log(process.platform)
+
+// 3. setTimeout(), clearTimeout(), setInterval, clearInterval()---global identifiers
+
+// setTimeout(()=>console.log("I am shahash"), 3000)
+
+// 4. require("")-------used to import the modules or packages installed from npm // we used to do import in react
+
+// 5. module.exports()-----for exporting the piece of code or files// we used to do export default in react 
